da38ee1669edf9c4d15a4f10e8209bf27b4458bb
jooms0212 <jooms0212@naver.com>
2025-08-12 09:08:58 +0900

    feat: 20250811 downcasting / Object / static

--- src/main/java/ch15_casting/centralcontrol/CentralControlMain.java
+++ src/main/java/ch15_casting/centralcontrol/CentralControlMain.java
@@ -6,6 +6,9 @@ public class CentralControlMain {
          Computer computer1 = new Computer();
          AirConditioner airConditioner1 = new AirConditioner();
          Speaker speaker1 = new Speaker();
+         Mouse mouse1 = new Mouse();
+         LED led1 = new LED();
+         Printer printer1 = new Printer();
          // 참조자료형의 배열을 생성(비어있는가)
         // 이하의 코드는 powers 생성하면서 indexNumber가 고정되기 때문에
         // 예시만 보여주고 안쓸겁니다.
@@ -13,7 +16,7 @@ public class CentralControlMain {
         // 저희는 그래서 centralControl 객체를 만들면서 내부의 필드에 비어있는 Power[] 배열을
         // 집어넣었습니다.
          CentralControl centralControl = new CentralControl(
-                 new Power[5]
+                 new Power[10]
          );
 
          centralControl.addDevice(computer1);       // 여기서 (암시적) 업캐스팅이 이루어졌습니다.
@@ -22,5 +25,23 @@ public class CentralControlMain {
          centralControl.addDevice(speaker1);
          centralControl.addDevice(speaker1);
          centralControl.addDevice(speaker1);
+         centralControl.addDevice(mouse1);
+         centralControl.addDevice(led1);
+         centralControl.addDevice(printer1);
+
+         centralControl.powerOn();
+         centralControl.powerOff();
+
+         centralControl.showInfo();
+
+         centralControl.performSpecificMethod();
+
+
+         /*
+               Mouse / LED 클래스를 정의하고 on() / off() 메서드를
+               implement 하시고
+               각 객체를 생성한 다음에
+               CentralControl 객체에 집어넣으시오
+          */
     }
 }
--- src/main/java/ch16_objects/ObjectTest.java
+++ src/main/java/ch16_objects/ObjectTest.java
@@ -1,4 +1,52 @@
 package ch16_objects;
 
 public class ObjectTest {
+    // 필드 선언
+    private String name;
+    private String address;
+
+    // 기본 생성자 / AllArgsConstructor를 정의하시오.
+    public ObjectTest() {}
+
+    public ObjectTest(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+    // getter / setter를 정의하시오.
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "이름 : " + name + "\n주소 : " + address;
+    }
+
+    // showInfo()를 call1() 형식으로 정의하시오.
+    /*
+        실행 예
+        이름 : 안근수
+        주소 : 부산광역시 연제구
+
+        Main으로 가서 objectTest1에
+        이름 안근수 주소 부산광역시 연제구를 set하고
+        showInfo()를 호출하시오.
+     */
+    void showInfo() {
+        System.out.println("이름 : " + getName());
+        System.out.println("주소 : " + getAddress());
+    }
 }
